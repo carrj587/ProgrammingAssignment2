4ea7d524ab5a4b8908787d6b541abe8051c35ee9
John Carr <carrj587@users.noreply.github.com>
2019-03-28 17:04:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Caching the Inverse of a Matrix:
+# Matrix inversion is usually a costly computation and there may be some 
+# benefit to caching the inverse of a matrix rather than compute it repeatedly.
+# Below are a pair of functions that are used to create a special object that 
+# stores a matrix and caches its inverse.
 
-## Write a short comment describing this function
+# This function creates a special "matrix" object that can cache its inverse.
+	## 1. set the value of the matrix
+	## 2. get the value of the matrix
+	## 3. set the value of inverse of the matrix
+	## 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                # <<- used to assign a value to an object in different environment
+				x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function (cacheSolve) computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        inv <- x$getInverse()
+        if (!is.null(inv)) { 	## if user had calculated the same matrix before
+                message("getting cached data")
+                return(inv)		## return old results directly
+        }
+        mat <- x$get()			## else, get the uncalculated matrix
+        inv <- solve(mat, ...)	## calculate the inverse matrix
+        x$setInverse(inv)		## reassign the calculated inverse matrix
+        inv						## prints the inverse matrix
+}								## End...
+   
